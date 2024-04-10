d04ec1a6c910ae20d04735064893712b2d925761
Viktoriya Lazareva <lazareva_vika2005@mail.ru>
2024-04-11 00:13:52 +0400

    fourth commit - interactive way to create the Fibonacci sequence that starts from 0, 1,...

---
+++ fibiter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void fib(int n){ //interactive way to create the Fibonacci sequence that starts from 0, 1,...
+    int current_element = 1; int previous_element = 1;
+    if (n > 1) {
+        cout << "0"<<"\n"<< "1"<<"\n";
+        for (int counter = 2; counter < n; ++counter){
+            cout << current_element << "\n";
+            current_element +=previous_element;
+            previous_element = current_element - previous_element;
+        }
+    }
+    else cout<<"0";
+}
+
+int main(){
+    int n; 
+    cout << "Enter n: "; cin >> n;
+    fib(n);
+}
+
